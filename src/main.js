475f891d64c4d4a5d41e736dccdfd66a253ad17f
益生菌 <2070159424@qq.com>
2024-10-10 15:20:38 +0800

    202410101520
    [add]增加了自动导入API和组件功能，可以减小构建后的文件大小。仅支持提供了自动导入方案的依赖。

--- src/main.js
+++ src/main.js
@@ -1,7 +1,4 @@
-import {createApp} from 'vue'
 import App from './App.vue'
 import router from "./router/index.js";
-import element from 'element-plus'
-import 'element-plus/dist/index.css'
 
-createApp(App).use(router).use(element).mount('#app')
+createApp(App).use(router).mount('#app')
