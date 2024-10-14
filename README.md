7ef23f1bfe947bfab93784bca4066e2be1e342b5
益生菌 <2070159424@qq.com>
2024-10-14 15:29:53 +0800

    202410141529
    [add]重写了README文档，增加了功能说明。
    [fix]更改了UUID生成功能的生成逻辑。由于内置API的安全性问题，现在采用uuid库进行计算，且能适合现在最新的标准生成。

--- README.md
+++ README.md
@@ -1,5 +1,56 @@
-# Vue 3 + Vite
+# 基于 Vue 和 SpringBoot技术的工具网站
 
-This template should help get you started developing with Vue 3 in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
+使用的框架：
 
-Learn more about IDE Support for Vue in the [Vue Docs Scaling up Guide](https://vuejs.org/guide/scaling-up/tooling.html#ide-support).
+前端：Vue + Vite
+
+后端：Spring
+
+
+
+使用到的依赖或库（通过npm list命令获取）：
+
+```
++-- @types/crypto-js@4.2.2
++-- @types/node-forge@1.3.11
++-- @vitejs/plugin-vue@5.1.4
++-- axios@1.7.7
++-- crypto-js@4.2.0
++-- element-plus@2.8.3
++-- file-type@19.5.0
++-- github-markdown-css@5.7.0
++-- markdown-it@14.1.0
++-- node-forge@1.3.1
++-- qr-code-styling@1.6.0-rc.1
++-- unplugin-auto-import@0.18.3
++-- unplugin-element-plus@0.8.0
++-- unplugin-vue-components@0.27.4
++-- uuid@10.0.0
++-- vite@5.4.7
++-- vue-router@4.4.5
+`-- vue@3.5.8
+```
+
+主要完成功能：
+
+简单的文字转图片、随机抽签、端口连通性测试、Markdown文件预览、简易二维码生成、文件/Base64互转、UUID生成、随机字符串生成、哈希计算、加密解密
+
+简单的文字转图片：通过canvas的特性实现。
+
+随机抽签：通过洗牌算法实现随机排序后抽取指定个数元素
+
+端口连通性测试：通过后端接口实现测试。
+
+Markdown文件预览：可通过输入源码或上传源文件读取源码。通过markdown-it库提供解析，使用github-markdown-css提供样式。
+
+简易二维码生成：通过qr-code-styling库实现简易二维码生成。实现了大部分的API。
+
+文件/Base64互转：通过上传文件自动转换。
+
+UUID生成：使用uuid库生成UUID，可生成v1、v3、v4、v5、v6、v7版本的UUID。可批量生成多个UUID，也可根据名称和命名空间生成指定UUID。
+
+随机字符串生成：根据自定义生成选择可实现生成指定长度、指定个数
+
+哈希计算：通过crypto-JS库实现计算MD5、SHA-1、SHA-224、SHA-256、SHA-384、SHA-512、SHA-3和RIPEMD160以及对应的Hmac值。
+
+加密解密。通过crypto-JS库实现AES对称算法的加密和解密，可完全自定义五个标准参数，但需要根据提示输入正确的参数才能执行。通过node-forge和@types/node-forge库实现RSA非对称算法的加密和解密，密钥可自动生成，可选择两种填充方式PKCS#1或OAEP并输入对应参数
--- src/components/A7.vue
+++ src/components/A7.vue
@@ -1,21 +1,77 @@
 <script>
+import {v1, v3, v4, v5, v6, v7} from "uuid";
+
 export default {
 	name: 'A7',
 	data() {
 		return {
 			/** 生成个数 */
-			count: 1
+			count: 1,
+			/** 选择生成版本。不同版本的特性。其中v1、v3、v5由于生成机制无法一次生成多个
+			 * v1：
+			 * 功能：生成基于时间戳和节点信息的 UUID（UUID v1）。无
+			 * 参数：无
+			 *
+			 * v3：
+			 * 功能：生成基于命名空间和名字的 UUID，使用 MD5 哈希（UUID v3）。根据命名空间和名字计算。
+			 * 参数：命名空间（本质是一个UUID），名字
+			 *
+			 * v4：
+			 * 功能：生成随机 UUID（UUID v4），通常是最常用的版本。
+			 * 参数：无
+			 * 可选参数：一个16个元素的，元素在0-255范围内的数组
+			 *
+			 * v5：
+			 * 功能：生成基于命名空间和名字的 UUID，使用 SHA-1 哈希（UUID v5）。
+			 * 参数：命名空间（本质是一个UUID），名字
+			 *
+			 * v6：
+			 * 功能：生成基于时间戳的 UUID（UUID v6），比 v1 版本更适合在高并发的环境中使用。
+			 * 参数：无
+			 *
+			 * v7：
+			 * 功能：基于时间戳的随机 UUID（UUID v7），是最新的版本之一，旨在提供更好的唯一性。
+			 * 参数：无
+			 * */
+			version: 'v4',
+			name: '',
+			namespace: ''
 		}
 	},
 	methods: {
+		createUUID() {
+			switch (this.version) {
+				case 'v1': {
+					return v1()
+				}
+				case 'v3': {
+					return v3(this.name, this.namespace)
+				}
+				case 'v4': {
+					return v4()
+				}
+				case 'v5': {
+					return v5(this.name, this.namespace)
+				}
+				case 'v6': {
+					return v6()
+				}
+				case 'v7': {
+					return v7()
+				}
+			}
+		},
 		/**
 		 * 生成指定个数的UUID，通过浏览器接口crypto实现。
 		 * */
 		create() {
 			document.getElementById('A7-container').innerHTML = ''
+			if (this.version === 'v1' || this.version === 'v3' || this.version === 'v5') {
+				this.count = 1
+			}
 			for (let i = 0; i < this.count; i++) {
 				const element = document.createElement('span');
-				element.innerText = crypto.randomUUID() + '\n'
+				element.innerText = this.createUUID() + '\n'
 				document.getElementById('A7-container').appendChild(element)
 			}
 		},
@@ -34,20 +90,35 @@ export default {
 
 <template>
 	<el-container>
-		<el-aside width="40%"/>
-		<el-aside width="30%">
+		<el-aside width="30%"/>
+		<el-aside width="40%">
 			<el-form label-width="auto">
-				<el-form-item label="生成个数">
-					<el-input-number v-model="count" :min="1" :step="1" :step-strictly="true"/>
+				<el-form-item label="生成个数" v-if="version==='v4'||version==='v6'||version==='v7'">
+					<el-input-number v-model="count" :min="1" :step="1" :step-strictly="true"/>&nbsp;
+					<el-text size="small" type="warning">生成个数过多时，请稍等</el-text>
+				</el-form-item>
+				<el-form-item label="版本">
+					<el-select v-model="version">
+						<el-option label="v1（基于时间戳和节点信息）" value="v1"/>
+						<el-option label="v3（基于命名空间和名字，MD5）" value="v3"/>
+						<el-option label="v4（随机，推荐）" value="v4"/>
+						<el-option label="v5（基于命名空间和名字，SHA-1）" value="v5"/>
+						<el-option label="v6（基于v1的高并发版）" value="v6"/>
+						<el-option label="v7（基于v1的最新版本）" value="v7"/>
+					</el-select>
+				</el-form-item>
+				<el-form-item label="名称" v-if="version==='v3'||version==='v5'">
+					<el-input v-model="name"/>
+					<el-text size="small" type="warning">相同的名称和命名空间会生成一样的UUID</el-text>
+				</el-form-item>
+				<el-form-item label="命名空间" v-if="version==='v3'||version==='v5'">
+					<el-input v-model="namespace"/>
+					<el-text size="small" type="primary">命名空间本质上还是一个UUID<br>预设值（固定值）：<br>DNS：6ba7b810-9dad-11d1-80b4-00c04fd430c8<br>URL：6ba7b811-9dad-11d1-80b4-00c04fd430c8</el-text>
 				</el-form-item>
 				<el-form-item>
 					<el-button @click="create" type="primary">生成</el-button>&nbsp;&nbsp;<el-button @click="download" type="success">下载</el-button>
 				</el-form-item>
 				<el-form-item>
-					<el-text type="info" size="small">
-						注：个数过多可能会卡顿，请稍候或减少个数
-						<hr style="height: 1px;color: black;width: 100%">
-					</el-text>
 					<div id="A7-container" style="line-height: normal;font-family: fangsong"></div>
 				</el-form-item>
 			</el-form>
